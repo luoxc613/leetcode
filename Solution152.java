e695ca672f4d76a78b8885a7cca4a3425730f9f4
luo <516408108@qq.com>
2017-04-13 21:46:27 +0800

    solution152

---
+++ Solution152.java
@@ -0,0 +1,16 @@
+public class Solution152 {
+    public int maxProduct(int[] nums) {
+        if(nums==null || nums.length==0)
+            return 0;
+        int max=nums[0],min=nums[0],result=nums[0];
+        for(int i=1; i<nums.length;i++)
+            {
+                int temp= max;
+                max=Math.max(Math.max(max*nums[i],min*nums[i]),nums[i]);
+                min=Math.min(Math.min(min*nums[i],temp*nums[i]),nums[i]);
+                if(result<max)
+                    result=max;
+            }
+    return result;
+    }
+}
