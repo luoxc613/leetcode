816dcb879b23b200ec3d184616a89ceaa1acc12b
luo <516408108@qq.com>
2017-04-13 13:37:06 +0800

    changes log

---
+++ First.java
@@ -0,0 +1,9 @@
+public class First {
+	public static void main(String[] args)
+	{
+		int x=3;
+		String name="luo xin cheng";
+		System.out.print("It is "+name );
+		System.out.println("my first java code!"+name);
+	}
+}
