d79e1f7460370edd2515812e5c1a2a437b21b79f
luoxc613 <luoxincheng_201605@yeah.net>
2017-09-05 12:07:38 +0800

    Create solution56.java

---
+++ solution56.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Interval> merge(List<Interval> intervals) {
+                Collections.sort(intervals, new Comparator<Interval>(){
+            @Override
+            public int compare(Interval obj0, Interval obj1) {
+                return obj0.start - obj1.start;
+            }
+        });
+
+        List<Interval> ret = new ArrayList<>();
+        Interval prev = null;
+        for (Interval inter : intervals) {
+            if (  prev==null || inter.start>prev.end ) {
+                ret.add(inter);
+                prev = inter;
+            } else if (inter.end>prev.end) {
+                prev.end = inter.end;
+            }
+        }
+        return ret;
+    }
+}
