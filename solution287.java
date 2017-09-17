c7ea168e0bfd274f3bce1af1b8754cdc3e01f4c0
luoxc613 <luoxincheng_201605@yeah.net>
2017-09-17 08:14:56 -0700

    Create solution287.java

---
+++ solution287.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+            Set <Integer> s= new HashSet<>();
+            for(int i=0;i<nums.length;i++)
+                if(!s.add(nums[i]))
+                    return nums[i];
+            return 0;
+    }
+}
